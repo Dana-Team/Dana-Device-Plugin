47d21bad1f3b61978a55c8d00c78b0ee3d3c8833
idanlevi215 <idanlevi215@gmail.com>
2020-12-29 18:23:48 +0200

    devsapce

--- deviceplugin/Allocation.go
+++ deviceplugin/Allocation.go
@@ -50,6 +50,7 @@ func (m *DanaDevicePlugin) Allocate(ctx context.Context, reqs *pluginapi.Allocat
 func (m *DanaDevicePlugin) GetPreferredAllocation(ctx context.Context, r *pluginapi.PreferredAllocationRequest) (*pluginapi.PreferredAllocationResponse, error) {
 	fmt.Print( " \n You Entered the GetPreferredAllocation FUNCTION \n")
 	response := &pluginapi.PreferredAllocationResponse{}
+	 fmt.Print("\n AvailableDeviceIDs  :" ,r.AvailableDeviceIDs[0],"\n")
 	for _, req := range r.ContainerRequests {
 		fmt.Print("\n req :  ",req ,"/n")
 		available, err := gpuallocator.NewDevicesFrom(req.AvailableDeviceIDs)
