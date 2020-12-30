3fd4e7016d76a1b2f5d2cc2d0ae390b36f8ebdac
idanlevi215 <idanlevi215@gmail.com>
2020-12-30 17:14:00 +0200

    devspace

--- cmd/deviceplugin.go
+++ cmd/deviceplugin.go
@@ -21,7 +21,7 @@ func getAllPlugins() []*dp.DanaDevicePlugin {
 			dp.NewGpuDeviceManager(),
 			"NVIDIA_VISIBLE_DEVICES" ,
 			gpuallocator.NewBestEffortPolicy(),
-			pluginapi.DevicePluginPath + "nvidia-dp2idan60.sock"),
+			pluginapi.DevicePluginPath + "nvidia-dp2idan61.sock"),
 	}
 }
 
--- deviceplugin/DanaDevicePlugin.go
+++ deviceplugin/DanaDevicePlugin.go
@@ -64,14 +64,9 @@ func (m *DanaDevicePlugin) DeviceExists(id string) bool {
 func (m *DanaDevicePlugin) ApiDevices() []*pluginapi.Device {
 	var pdevs []*pluginapi.Device
 	for _, d := range m.cachedDevices {
-		var fakeID string
-		var fakeDevice pluginapi.Device
-		for j := 0 ; j <3 ; j++{
-			var s string
-			fakeDevice = d.Device
-			s=strconv.Itoa(j)
-			fakeID = d.Device.ID + s
-			fakeDevice.ID = fakeID
+		fakeDevice := d.Device
+		for j := 0 ; j <2 ; j++{
+			fakeDevice.ID = fakeDevice.ID + "-" + strconv.Itoa(j)
 			pdevs = append(pdevs, &fakeDevice)
 		}
 	}
